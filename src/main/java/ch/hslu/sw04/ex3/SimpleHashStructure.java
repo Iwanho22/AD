1929b128872d624b23abf86186f4a5e1987c9ba8
Ivo von Rickenbach <ivo.vonrickenbach@stud.hslu.ch>
2024-03-19 19:34:57 +0100

    SW04

---
+++ src/main/java/ch/hslu/sw04/ex3/SimpleHashStructure.java
@@ -0,0 +1,71 @@
+package ch.hslu.sw04.ex3;
+
+import java.util.Arrays;
+
+/**
+ * This is a simple implementation of the hash structure.
+ * It does not allow to store null references.
+ *
+ * @param <E> The type of the element to be stored.
+ */
+public final class SimpleHashStructure<E> implements HashStructure<E> {
+    public static final int DEFAULT_CAPACITY = 10;
+
+    private final Object[] data;
+    private int size;
+
+    public SimpleHashStructure() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public SimpleHashStructure(int capacity) {
+        data = new Object[capacity];
+    }
+
+    @Override
+    public boolean add(E value) {
+        int index = getIndex(value);
+
+        if (data[index] == null) {
+            data[index] = value;
+            size++;
+        } else{
+
+        }
+
+        return true;
+    }
+
+    private int getIndex(E value) {
+        return value.hashCode() % data.length;
+    }
+
+    @Override
+    public boolean remove(E value) {
+        var index = getIndex(value);
+        if (data[index] != null) {
+            data[index] = null;
+            size--;
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean contains(E value) {
+        return data[getIndex(value)] != null && data[getIndex(value)].equals(value);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleHashStructure{" +
+                "data=" + Arrays.toString(data) +
+                '}';
+    }
+}
